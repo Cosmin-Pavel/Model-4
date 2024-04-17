afa66d8015c17e8c953dca881213090251adde57
cozminCrima <126168168+cozminCrima@users.noreply.github.com>
2024-04-17 13:58:32 +0300

    fixed plots

--- main.py
+++ main.py
@@ -278,35 +278,35 @@ def evaluate_model(model, criterion, test_loader, device):
     test_mae = running_mae / total_samples
     return test_loss_mse, test_loss_mae, test_mae
 
-def plot_data(train_loader, model, device, optimizer, num_epochs, batch_size, lr, trainSize, testSize, name):
+def plot_data(dataset, model, device, optimizer, num_epochs, batch_size, lr, trainSize, testSize, name):
     model.eval()
     with torch.no_grad():
-        for inputs, labels in train_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs).cpu().numpy()
+        inputs, labels = dataset[:]
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs).cpu().numpy()
 
-            plt.figure(figsize=(10, 6))
-            plt.scatter(range(len(labels)), labels.cpu().numpy(), label='Actual Evaluation', marker='o')
-            plt.scatter(range(len(outputs)), outputs, label='Predicted Evaluation', marker='x')
-            plt.xlabel('Data Index')
-            plt.ylabel('Evaluation')
-            plt.title('Actual vs Predicted Evaluation')
-            plt.legend()
+        plt.figure(figsize=(10, 6))
+        plt.scatter(range(len(labels)), labels.cpu().numpy(), label='Actual Evaluation', marker='o')
+        plt.scatter(range(len(outputs)), outputs, label='Predicted Evaluation', marker='x')
+        plt.xlabel('Data Index')
+        plt.ylabel('Evaluation')
+        plt.title('Actual vs Predicted Evaluation')
+        plt.legend()
 
-            # Add model information as text on the right side
-            text = f"Optimizer: {optimizer}\nEpochs: {num_epochs}\nBatch Size: {batch_size}\nLearning Rate: {lr}\nTraining Data Size: {int(trainSize)}\nTest Data Size: {int(testSize)}"
-            plt.text(1.02, 0.5, text, horizontalalignment='left', verticalalignment='center', transform=plt.gca().transAxes)
+        # Add model information as text on the right side
+        text = f"Optimizer: {optimizer}\nEpochs: {num_epochs}\nBatch Size: {batch_size}\nLearning Rate: {lr}\nTraining Data Size: {int(trainSize)}\nTest Data Size: {int(testSize)}"
+        plt.text(1.02, 0.5, text, horizontalalignment='left', verticalalignment='center', transform=plt.gca().transAxes)
 
-            # Adjust layout to fit the text and plot within the image
-            plt.subplots_adjust(right=0.7)  # Adjust the space on the right for the text
-            plt.margins(0.2)  # Add some margin around the plot
+        # Adjust layout to fit the text and plot within the image
+        plt.subplots_adjust(right=0.7)  # Adjust the space on the right for the text
+        plt.margins(0.2)  # Add some margin around the plot
 
-            # Generate a unique filename with timestamp
-            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-            filename_mse = f'plots/ValuesGraph-{name}-{timestamp}.png'
-            plt.savefig(filename_mse)
-            plt.show()
-            break
+        # Generate a unique filename with timestamp
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename_mse = f'plots/ValuesGraph-{name}-{timestamp}.png'
+        plt.savefig(filename_mse)
+        plt.show()
+
 
 file_path = FILE_PATH
 skip_rows = SKIP_ROWS
@@ -325,8 +325,12 @@ class ChessDataset(Dataset):
         return len(self.data_frame) // self.batch_size
 
     def __getitem__(self, index):
-        start_idx = index * self.batch_size
-        end_idx = (index + 1) * self.batch_size
+        if isinstance(index, slice):
+            start_idx, end_idx = index.start, index.stop
+        else:
+            start_idx = index * self.batch_size
+            end_idx = (index + 1) * self.batch_size
+
         batch_data = self.data_frame.iloc[start_idx:end_idx]
         fens = torch.from_numpy(np.array([*map(fen_to_bit_vector, batch_data["FEN"])], dtype=np.float32))
 
